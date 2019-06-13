63a6018cfa08222e39af25410a3a73911cff7282
Nick Marino <nick.marino126@gmail.com>
2019-06-13 15:12:58 -0400

    refactored

--- weatherbox_display/weatherbox_display/weatherbox_client/weatherbox_client.py
+++ weatherbox_display/weatherbox_display/weatherbox_client/weatherbox_client.py
@@ -4,15 +4,6 @@ import yaml
 ''' Weatherbox configuration file path '''
 weatherbox_config = 'weatherbox_client/config/weatherbox-config.yaml'
 
-''' Weatherbox Enpoints '''
-subscribe = '/web/subscribe'
-full = '/web/getFull'
-currently = '/web/getCurrently'
-minutely = '/web/getMinutely'
-hourly = '/web/getHourly'
-daily = '/web/getDaily'
-alerts = '/web/getAlerts'
-
 class WeatherboxClient:
     ''' A client for communicating with the Weatherbox service. '''
     
@@ -20,7 +11,11 @@ class WeatherboxClient:
         with open(weatherbox_config, 'r') as stream:
             try: 
                 config = yaml.load(stream)
-                self.endpoint = config['endpoint']
+                self.baseEndpoint = config['baseEndpoint']
+
+                self.endpoints = {}
+                for ep in config['endpoints']['web']:
+                    self.endpoints.update(ep)
 
                 options = {}
                 for opt in config['options']:
@@ -32,33 +27,37 @@ class WeatherboxClient:
     
     def _subscribe(self, options):
         ''' Subscribes the client to the Weatherbox service '''
-        endpoint = self.endpoint + subscribe
+        endpoint = self.baseEndpoint + self.endpoints['subscribe']
         clientId = requests.post(endpoint, json=options).text
         return clientId
 
     def _getForecast(self, forecastType):
         ''' Convenience method for get forecast data '''
-        endpoint = self.endpoint + forecastType
+        endpoint = self.baseEndpoint + forecastType
         params = {'clientId': self.clientId}
         forecast = requests.get(endpoint, params=params).json()
         return forecast
     
     def getFull(self):
         ''' Gets the full forecast '''
-        return self._getForecast(full)
+        return self._getForecast(self.endpoints['full'])
 
     def getCurrently(self):
         ''' Gets the current forecast '''
-        return self._getForecast(currently)
+        return self._getForecast(self.endpoints['currently'])
+
+    def getDaily(self):
+        ''' Gets the daily forecast '''
+        return self._getForecast(self.endpoints['daily'])
 
     def getMinutely(self):
         ''' Gets the minutely forecast '''
-        return self._getForecast(minutely)
+        return self._getForecast(self.endpoints['minutely'])
     
     def getHourly(self):
         ''' Gets the hourly forecast '''
-        return self._getForecast(hourly)
+        return self._getForecast(self.endpoints['hourly'])
 
     def getAlerts(self):
         ''' Gets the alerts '''
-        return self._getForecast(alerts)
+        return self._getForecast(self.endpoints['alerts'])
